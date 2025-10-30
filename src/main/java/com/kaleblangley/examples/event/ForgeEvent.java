c6bb634bfdd058d98150a503d9db36df448fafd5
Kaleb Langley <109327256+KLjiana@users.noreply.github.com>
2025-10-30 13:11:36 +0800

    Initial commit

---
+++ src/main/java/com/kaleblangley/examples/event/ForgeEvent.java
@@ -0,0 +1,8 @@
+package com.kaleblangley.examples.event;
+
+import com.kaleblangley.examples.Examples;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Examples.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class ForgeEvent {
+}
